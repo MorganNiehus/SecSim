09a41f71c4b84eac351c47ef387c979231da8b0f
Morgan <nihuspawns@gmail.com>
2016-02-02 11:48:42 -0500

    Doc

--- hw_2MNiehus/src/prob3/Employee.java
+++ hw_2MNiehus/src/prob3/Employee.java
@@ -2,8 +2,8 @@
 //Name: Morgan Niehus
 //Student ID: 870537844
 //Class: CS 1302-A
-//HW: HomeWork 1
-//Due Date: 1/27/16
+//HW: HomeWork 2
+//Due Date: 2/3/16
 //************************
 package prob3;
 
