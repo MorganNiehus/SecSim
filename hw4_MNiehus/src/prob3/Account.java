9bdda4cad707986aba0f2ba1f6bd5ecf79c61b48
unknown <maniehus@IT13434.valdosta.edu>
2016-02-22 10:18:13 -0500

    ...

---
+++ hw4_MNiehus/src/prob3/Account.java
@@ -0,0 +1,42 @@
+package prob3;
+
+public abstract class Account {
+	
+	private double balance = 0;
+	
+	protected Account(){
+		
+	}
+	protected Account(double balance){
+		this.balance = balance;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public double deposit(double amount){
+		
+		return balance += amount;
+	}
+	
+	public double withdraw(double amount){
+		return balance -= amount;
+	}
+	
+	public String toString(){
+		String result = " ";
+		result = "The balance is: " + getBalance();
+		return result;
+	}
+	
+	public abstract double applyInterest();
+	
+	
+	
+	
+
+}
